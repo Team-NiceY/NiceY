aca24aa60ec88c56d62e05e2ebbcb0d7d229dcc2
SeongHo Cho <139312827+Cho-yool@users.noreply.github.com>
2023-11-03 13:56:33 +0900

    Initial commit

---
+++ src/main/java/site/nicey/nicey/NiceyApplication.java
@@ -0,0 +1,13 @@
+package site.nicey.nicey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiceyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/nicey/nicey/NiceyApplicationTests.java
@@ -0,0 +1,13 @@
+package site.nicey.nicey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiceyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
